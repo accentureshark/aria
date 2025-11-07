101ac80722dc380a2ed90fcda3f3dab9f1fa277a
faguero <faguero@gmail.com>
2025-11-07 20:39:36 -0300

    directory refactor

---
+++ src/main/java/com/accenture/aria/model/Priority.java
@@ -0,0 +1,8 @@
+package com.accenture.aria.model;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH,
+    URGENT
+}
---
+++ src/main/java/com/accenture/aria/model/Status.java
@@ -0,0 +1,8 @@
+package com.accenture.aria.model;
+
+public enum Status {
+    OPEN,
+    IN_PROGRESS,
+    RESOLVED,
+    CLOSED
+}
